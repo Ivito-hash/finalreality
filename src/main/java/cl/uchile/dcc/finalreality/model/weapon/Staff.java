da5651bfe21aaa3a928ee79e355876f2b35b6dd4
ivofc <ivo.fc21@gmail.com>
2022-09-28 18:28:43 -0300

    feat: Add a class to staff type

---
+++ src/main/java/cl/uchile/dcc/finalreality/model/weapon/Staff.java
@@ -0,0 +1,75 @@
+/*
+ * "Final Reality" (c) by R8V and ~Ivo Fuenzalida~
+ * "Final Reality" is licensed under a
+ * Creative Commons Attribution 4.0 International License.
+ * You should have received a copy of the license along with this
+ * work. If not, see <http://creativecommons.org/licenses/by/4.0/>.
+ */
+
+package cl.uchile.dcc.finalreality.model.weapon;
+
+import cl.uchile.dcc.finalreality.exceptions.InvalidStatValueException;
+import cl.uchile.dcc.finalreality.exceptions.Require;
+import cl.uchile.dcc.finalreality.model.character.player.PlayerCharacter;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+
+/**
+ * A {@link PlayerCharacter} that can equip {@code Axe}s and {@code Bow}s.
+ *
+ * @author <a href="https://www.github.com/r8vnhill">R8V</a>
+ * @author ~Ivo Fuenzalida~
+ */
+public class Staff extends Weapon {
+
+  private final int magic;
+
+  /**
+   * Creates a new bow.
+   *
+   * @param name
+   *     the character's name
+   * @param damage
+   *     the character's max hp
+   * @param magic damage
+   *     the character's max hp
+   * @param weight
+   *     the character's defense
+   * @param type
+   *     the queue with the characters waiting for their turn
+   */
+  public Staff(final @NotNull String name, final int damage, int magic,
+               final int weight, final WeaponType type) throws InvalidStatValueException{
+    super(name, damage, weight, type);
+    Require.statValueAtLeast(1, magic, "Magic");
+    this.magic = magic;
+  }
+
+  @Override
+  public boolean equals(final Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof final Staff that)) {
+      return false;
+    }
+    return hashCode() == that.hashCode()
+            && name.equals(that.name)
+            && damage == that.damage
+            && magic == that.magic
+            && weight == that.weight
+            && type == that.type;
+  }
+
+  @Override
+  public String toString() {
+    return "Staff{name='%s', damage=%d, magic=%d, weight=%d, type=%s}"
+            .formatted(name, damage, magic, weight, type);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(Staff.class, name, damage, magic, weight, type);}
+}
